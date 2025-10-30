9ff5b11e764ffb01124918f3771f61bb4efb614a
Rohit Maurya <102695143+rohit8168@users.noreply.github.com>
2025-10-30 19:45:13 +0530

    Add PORT environment variable to settings

--- resume_extractor/resume_extractor/settings.py
+++ resume_extractor/resume_extractor/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+PORT = os.environ.get("PORT", "8000")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
