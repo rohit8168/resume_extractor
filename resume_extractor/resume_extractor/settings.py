b8c799d1e93f53cef684c24bc7f25f110c2144b9
Rohit Maurya <102695143+rohit8168@users.noreply.github.com>
2025-10-30 19:36:26 +0530

    Update settings.py

--- resume_extractor/resume_extractor/settings.py
+++ resume_extractor/resume_extractor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4+a=@g^bvs9u_+glfop(un(gf-&fj%149qcipilduy49flyg2y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
