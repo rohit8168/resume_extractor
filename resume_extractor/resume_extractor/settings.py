06958579493eeb37972a67ffa41c559505837512
Rohit Maurya <102695143+rohit8168@users.noreply.github.com>
2025-10-30 21:02:26 +0530

    Add CSRF trusted origins for deployment

--- resume_extractor/resume_extractor/settings.py
+++ resume_extractor/resume_extractor/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://resume-extractor-ae3r.onrender.com',
+]
+
